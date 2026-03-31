0dec5b1b5106b5a22dece41e1d9432a494aaf0f1
Ayesha Rashid <107069755+Ayesha-Rashid2@users.noreply.github.com>
2026-03-31 11:38:26 -0400

    Add: lru-cache - submission-3

---
+++ Data Structures & Algorithms/lru-cache/submission-3.py
@@ -0,0 +1,42 @@
+from collections import OrderedDict
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        
+        self.cache.move_to_end(key) 
+        return self.cache[key]
+        
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache[key] = value
+            self.cache.move_to_end(key)
+        else:
+            if len(self.cache) >= self.capacity: 
+                self.cache.popitem(last=False)
+            self.cache[key] = value 
+            self.cache.move_to_end(key)
+    
+    def delete(self, key: int) -> bool:
+        if key in self.cache:
+            del self.cache[key]
+            return True
+        return False
+    
+    def last(self) -> int:
+        if not self.cache:
+            return -1
+        key, value = self.cache.popitem(last=False) 
+        self.cache[key] = value                      
+        self.cache.move_to_end(key, last=False)     
+        return value
+
+        
