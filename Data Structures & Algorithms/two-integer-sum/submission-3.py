202dc1d0643ce9650eab73b6be72e560d166bcd8
Ayesha Rashid <107069755+Ayesha-Rashid2@users.noreply.github.com>
2026-04-03 19:43:47 -0400

    Update: two-integer-sum - submission-3

--- Data Structures & Algorithms/two-integer-sum/submission-3.py
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -1,6 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:   
         
+        '''
         myMap = {}
         for i, num in enumerate(nums):
             comp = target - num
@@ -8,3 +9,11 @@ class Solution:
                 return [myMap[comp], i]
             else:
                 myMap[num] = i
+
+        '''
+
+        for i in range(len(nums)):
+            comp = target - nums[i]
+            for j in range(i+1, len(nums)):
+                if nums[j] == comp:
+                    return [i, j]
