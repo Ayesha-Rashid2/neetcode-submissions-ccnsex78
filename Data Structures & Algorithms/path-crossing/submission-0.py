631001dea2bee5ba1bb29b52f4355da0703e8a11
Ayesha Rashid <107069755+Ayesha-Rashid2@users.noreply.github.com>
2026-04-04 00:00:14 -0400

    Add: path-crossing - submission-0

---
+++ Data Structures & Algorithms/path-crossing/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        location = {(0,0)}
+        x, y = 0, 0
+        for direction in path:
+            if direction == 'N':
+                y += 1
+            elif direction == 'S':
+                y -= 1
+            elif direction == 'E':
+                x += 1
+            else:
+                x -= 1
+
+            if (x,y) in location:
+                return True
+            location.add((x,y))
+        return False
