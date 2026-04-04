041e73cee03731e7c9d6363304cc750cedb8c5db
Ayesha Rashid <107069755+Ayesha-Rashid2@users.noreply.github.com>
2026-04-04 18:44:24 -0400

    Add: intersection-of-two-arrays - submission-0

---
+++ Data Structures & Algorithms/intersection-of-two-arrays/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1_set = set()
+        nums2_set = set()
+        res = []
+
+        for nums in nums1:
+            if nums not in nums1_set:
+                nums1_set.add(nums)
+
+        for nums in nums2:
+            if nums not in nums2_set:
+                nums2_set.add(nums)
+
+        
+        for nums in nums1_set:
+            if nums in nums2_set:
+                res.append(nums)
+        
+        return res
+        
+        
