3610f7d10e079c249280d3a5aa8351b5e7bcca64
Ayesha Rashid <107069755+Ayesha-Rashid2@users.noreply.github.com>
2026-04-09 17:10:25 -0400

    Update: concatenation-of-array - submission-3

--- Data Structures & Algorithms/concatenation-of-array/submission-3.py
+++ Data Structures & Algorithms/concatenation-of-array/submission-3.py
@@ -1,6 +1,6 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        nums_2 = []
+        #nums_2 = []
         '''
         for i in range(len(nums)):
             nums_2.append(nums[i])
@@ -8,13 +8,21 @@ class Solution:
         for i in range(len(nums)):
             nums_2.append(nums[i])
         
-        '''
+        
         for i in range(2):
             for n in nums:
                 nums_2.append(n)
 
+        '''
+        nums2 = []
+        for i in range(2):
+            for num in nums:
+             nums2.append(num)
+
+        return nums2
 
 
-        return nums_2            
+
+
 
         
